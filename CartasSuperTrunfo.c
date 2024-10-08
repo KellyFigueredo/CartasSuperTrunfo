345e6319b57ce8d3d9510bf4da8b78f58b5fc40c
KellyFigueredo <figueredo.kellyy@gmail.com>
2024-10-08 21:41:33 +0000

    acrescentei a soam

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,52 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  int estado;
+  int codigo;
+  char cidade[50];
+  int populaçao;
+  float area;
+  float densidade;
+  float pib;
+  int capita;
+  int turistico;
+  float media;
+
   // Área para entrada de dados
 
+  printf ("Digite o estado: \n");
+  scanf ("%d", &estado);
+
+  printf ("Digite o código da carta: \n");
+  scanf ("%d", &codigo);
+
+  printf ("Digite o nome da cidade: \n");
+  scanf ("%s", &cidade);
+
+  printf ("Digite a população: \n");
+  scanf ("%d", &populaçao); 
+
+  printf ("Digite o área: \n");
+  scanf ("%.2f", &area);
+
+  printf ("Digite a densidade populacional: \n");
+  scanf ("%.2f", &densidade);
+
+  printf ("Digite o PIB: \n");
+  scanf ("%.2f", &pib);
+
+  printf ("Digite o PIB per Capita: \n");
+  scanf ("%d", &capita);
+
+  printf ("Digite o número de pontos turísticos: \n");
+  scanf ("%d", &turistico);
+
   // Área para exibição dos dados da cidade
+  // soma da Densidade Populacional e o PIB per Capita
+
+  media = ( densidade + capita );
+
+  printf("A soma é: %.2f", media);
 
 return 0;
 }
